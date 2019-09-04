559a91ee7fb762854c31162213f3cf15a1669a50
Jake Moening <jmoening@imagetrend.com>
2019-09-04 13:41:59 -0500

    Initial Commit of asp.net core project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ynab_or_your_life.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
