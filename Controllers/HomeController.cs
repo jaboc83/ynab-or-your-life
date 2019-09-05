459429cf97bbf0411ebf33cedd2aaf48b3bd566f
Jake Moening <jmoening@imagetrend.com>
2019-09-05 14:50:36 -0500

    Add start of OAuth for hosting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,21 +8,22 @@ using YNABOrYourLife.Models;
 
 namespace YNABOrYourLife.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      ViewData.Add("ClientId", "63b4364465cb3e94af96e1475d986911d55cd24634b583787257ddb546f78a7c");
+      return View();
     }
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
